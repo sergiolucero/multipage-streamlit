2418392f3f9c583b4e275b87dcfe7199293e156a
Sergio Lucero <sergiolucero@gmail.com>
2022-06-05 10:29:17 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,22 +1,9 @@
-# Copyright 2018-2022 Streamlit Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+pending = ['mapabicis','gasolina','television','greenpeace','IPSA']
 
 def run():
     st.set_page_config(
@@ -25,15 +12,11 @@ def run():
     )
 
     st.write("# Welcome to Streamlit! 👋")
-
+    st.write(pending)
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
         ### Want to learn more?
         - Check out [streamlit.io](https://streamlit.io)
         - Jump into our [documentation](https://docs.streamlit.io)
