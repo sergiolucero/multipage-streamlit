0c8236e9052720dc6f4bda45a3adb79d5cd98002
Sergio Lucero <sergiolucero@gmail.com>
2022-06-05 23:14:03 -0400

    Update 4_lucero.py

--- pages/4_lucero.py
+++ pages/4_lucero.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import boto3
 import glob
 
 st.header('Quant Portfolio')
 
 for fn in sorted(glob.glob('pages/*.png')):
     st.image(fn, caption=fn, use_column_width='always')
+
+uploaded_file = st.file_uploader("upload picture")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     # upload to github
+     
