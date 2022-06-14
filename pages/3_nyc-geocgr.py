6e1c6577496c20a13097cc471bd55906125bfe61
Sergio Lucero <sergiolucero@gmail.com>
2022-06-13 20:35:05 -0400

    Update 3_nyc-geocgr.py

--- pages/3_nyc-geocgr.py
+++ pages/3_nyc-geocgr.py
@@ -12,9 +12,9 @@ def load_data():
     data = pd.read_csv(
         "ViñaFinal_pts.csv", #"uber-raw-data-sep14.csv.gz",
         #nrows=100000,  # approx. 10% of data
-        names=["descripción","monto","lat","lon",],  # specify names directly since they don't change
+        #names=["descripción","monto","lat","lon",],  # specify names directly since they don't change
         #skiprows=1,  # don't read header since names specified directly
-        usecols=[1,2,77,78], # descripcion,monto,lat,lon
+        #usecols=[1,2,77,78], # descripcion,monto,lat,lon
         #usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
         #parse_dates=["date/time"],  # set as datetime instead of converting after the fact
     )
